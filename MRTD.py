a540276240d08fa101343a19234cf7b26147e6db
Tyler-Seliber <tylerseliber@gmail.com>
2022-12-04 15:43:59 -0500

    check fields with check digits

--- MRTD.py
+++ MRTD.py
@@ -1,7 +1,69 @@
-# # Helper function to check fields and check digits
-# def checkField(field, digits):
-#     '''Checks the field and returns True if it is valid and False otherwise'''
-#     pass
+# Dictionary to map digits to numeric values for check digit calculation
+digit_map = {
+    '<': 0,
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'A': 10,
+    'B': 11,
+    'C': 12,
+    'D': 13,
+    'E': 14,
+    'F': 15,
+    'G': 16,
+    'H': 17,
+    'I': 18,
+    'J': 19,
+    'K': 20,
+    'L': 21,
+    'M': 22,
+    'N': 23,
+    'O': 24,
+    'P': 25,
+    'Q': 26,
+    'R': 27,
+    'S': 28,
+    'T': 29,
+    'U': 30,
+    'V': 31,
+    'W': 32,
+    'X': 33,
+    'Y': 34,
+    'Z': 35
+}
+# Weighting for the check digit calculation
+weightings = [7, 3, 1]
+
+# Helper function to check fields and check digits
+def checkField(field, provided_digit):
+    '''Checks the field and returns True if it is valid and False otherwise'''
+    field_splice = list(field)
+    # Assign numeric values to each character in the field
+    numeric_values = list(map(lambda x: digit_map[x], field_splice))
+
+    # Assign weights to the digits
+    for i in range(len(numeric_values)):
+        numeric_values[i] *= weightings[i % len(weightings)]
+
+    # Sum the weighted digits
+    numeric_sum = sum(numeric_values)
+
+    # Calculate the check digit
+    calculated_digit = numeric_sum % 10
+
+    # Check if the calculated digit matches the provided digit
+    if calculated_digit == provided_digit:
+        return 'Passed'
+    else:
+        return 'Failed'
+     
 
 # Requirement 1
 def scanMRZ(doc):
@@ -20,7 +82,6 @@ def decodeMRZ(line1, line2):
     del list1[1] # Remove the country code and last name combo from the list
     list1.insert(1, country_code)
     list1.insert(2, last_name)
-    print(list1)
 
     # Decode line2
     # Split the line into a list of strings
@@ -35,8 +96,6 @@ def decodeMRZ(line1, line2):
     list2.append(line2[27:28]) # Check digit for expiration date
     list2.append(line2[28:-1]) # Personal number
     list2.append(line2[-1:]) # Check digit for personal number
-
-    print(list2)
 
     # Combine the two lists into a dictionary
     traveler_info = {
@@ -64,14 +123,24 @@ def encodeTravelInfo(dbEntry):
     pass
 
 # Requirement 4
-def checkMRZ(line1, line2):
+def checkMRZ(traveler_info):
     '''Checks the MRZ and returns True if it is valid and False otherwise'''
-    pass
+    failed_checks = {
+        'Passport Number': checkField(traveler_info['passport_number'], int(traveler_info['passport_number_check_digit'])),
+        'Birth Date': checkField(traveler_info['birth_date'], int(traveler_info['birth_date_check_digit'])),
+        'Expiration Date': checkField(traveler_info['expiration_date'], int(traveler_info['expiration_date_check_digit'])),
+        'Personal Number': checkField(traveler_info['personal_number'], int(traveler_info['personal_number_check_digit']))
+    }
+    return failed_checks
 
 def main():
     line1 = 'P<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<'
-    line2 = 'L898902036UT07408122F1204159ZE184226B<<<<<<1'
-    print(decodeMRZ(line1, line2))
+    line2 = 'L898902C36UT07408122F1204159ZE184226B<<<<<<1'
+
+    info = decodeMRZ(line1, line2)
+    print('Traveler info: ' + str(info))
+    print('Check results: ' + str(checkMRZ(info)))
+
 
 if __name__ == '__main__':
     main()
